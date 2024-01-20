24462aa15f1ef41ae78acb1c53d2847759fa4af2
Cheyenne Pantoja <CheyennePantoja@gmail.com>
2024-01-19 20:10:35 -0800

    added functions

---
+++ server/db/sqlHelperFunctions/users.js
@@ -0,0 +1,49 @@
+const client = require('./client');
+const util = require('util');
+
+// POST - api/users/register - create a new user
+async function createUser(body) {
+    try {
+        const { rows: [user] } = await client.query(`
+        INSERT INTO users(name, email)
+        VALUES($1, $2)
+        RETURNING *;
+        `, [body.name, body.email]);
+        return user;
+    } catch (error) {
+        throw error;
+    }
+}
+
+// POST - api/users/login - login a user
+async function loginUser(body) {
+    try {
+        const { rows: [user] } = await client.query(`
+        INSERT INTO users(email, password)
+        VALUES($1, $2)
+        RETURNING *;
+        `, [body.email, body.password]);
+        return user;
+    } catch (error) {
+        throw error;
+    }
+}
+
+// GET - /api/users/:id - get a single user by id
+async function getUserById(id) {
+    try {
+        const { rows: [user] } = await client.query(`
+            SELECT * FROM users
+            WHERE id = $1;
+        `, [id]);
+        return user;
+    } catch (error) {
+        throw error;
+    }
+}
+
+module.exports = {
+    createUser,
+    loginUser,
+    getUserById
+}
