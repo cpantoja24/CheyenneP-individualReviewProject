6f3f81fb219cda7ed5699378c11af0be60acc3fb
Cheyenne Pantoja <CheyennePantoja@gmail.com>
2024-01-25 12:24:18 -0800

    cleaned up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,6 @@ function App() {
   return (
     <>
       <div><NavBar/> </div>
-      <p>Welcome</p>
     </>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-// import store from './redux/store'
+import { store } from './components/Recipes.jsx'
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
+     <BrowserRouter>
+     <Provider store={store}>
         <App />
-      </BrowserRouter>
-    </Provider>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
