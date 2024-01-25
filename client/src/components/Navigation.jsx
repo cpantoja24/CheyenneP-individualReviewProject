3f2d392a11f2a2f0687b4c38b63ad3a29e3cbfb5
Cheyenne Pantoja <CheyennePantoja@gmail.com>
2024-01-25 12:33:06 -0800

    cleaned up

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,17 +1,23 @@
 import { Routes, Route, Link } from 'react-router-dom'
+import Recipes from './Recipes'
+import SingleRecipe from './SingleRecipe'
+import Ingredients from './Ingredients'
 
 export default function NavBar() {
 
     return (
         <>
-        <p>Welcome</p>
-            {/* <div id="navbar">
-                <Link to={'/'}>Home</Link>
+            <p>Welcome</p>
+            <div id="navbar">
+                <Link to={'/'}>Recipes</Link>
+                <br />
+                <Link to={'/ingredients'}>Ingredients</Link>
             </div>
             <Routes>
                 <Route path='/' element={<Recipes />} />
-            </Routes> */}
-
+                <Route path='/recipes/:recipeId' element={<SingleRecipe />} />
+                <Route path='/ingredients' element={<Ingredients />} />
+            </Routes>
         </>
     )
 }
