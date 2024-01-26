1fd38bc594648c6aba39cea97446c6947b70fc0f
Cheyenne Pantoja <CheyennePantoja@gmail.com>
2024-01-26 15:20:36 -0800

    updated functions

--- client/src/fetching.js
+++ client/src/fetching.js
@@ -3,21 +3,10 @@ const BASE_URL = `http://localhost:8080/api`;
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-
+// Recipes
 export const fetchAllRecipes = async () => {
     try {
         const response = await fetch(`${BASE_URL}/recipes`)
-        const result = await response.json();
-        console.log(result);
-        return result
-    } catch (err) {
-        console.error(err);
-    }
-}
-
-export const fetchAllIngredients = async () => {
-    try {
-        const response = await fetch(`${BASE_URL}/ingredients`)
         const result = await response.json();
         console.log(result);
         return result
@@ -43,24 +32,6 @@ export const fetchSingleRecipe = async (recipeId) => {
     }, [])
 };
 
-export function SingleIngredient() {
-    const { ingredientId } = useParams()
-
-    useEffect(() => {
-        async function fetchSingleIngredient() {
-            try {
-                const response = await fetch(`http://localhost:8080/api/ingredients/${ingredientId}`);
-                const ingredient = await response.json();
-                console.log(ingredient)
-
-            } catch (error) {
-                console.error('Uh oh, trouble fetching single ingredient!', error);
-            }
-        }
-        fetchSingleIngredient()
-    }, [])
-};
-
 export const addNewRecipe = async (recipe) => {
     try {
         const response = await fetch(`${BASE_URL}/recipes`, {
@@ -81,17 +52,66 @@ export const addNewRecipe = async (recipe) => {
     }
 };
 
+
 export const removeRecipe = async (recipeId) => {
     try {
-        const response = await fetch(`${BASE_URL}/${recipeId}`, {
+        const response = await fetch(`${BASE_URL}/recipes/${recipeId}`, {
             method: "DELETE"
         });
+        const result = await response.json();
+        window.location.reload();
+        return result
+        
+    } catch (err) {
+        console.error(
+            `Whoops, trouble removing recipe #${recipeId}!`,
+            err
+        );
+    }
+};
+
+// Ingredients
+export const fetchAllIngredients = async () => {
+    try {
+        const response = await fetch(`${BASE_URL}/ingredients`)
         const result = await response.json();
         console.log(result);
         return result
     } catch (err) {
+        console.error(err);
+    }
+};
+
+export function SingleIngredient() {
+    const { ingredientId } = useParams()
+
+    useEffect(() => {
+        async function fetchSingleIngredient() {
+            try {
+                const response = await fetch(`http://localhost:8080/api/ingredients/${ingredientId}`);
+                const ingredient = await response.json();
+                console.log(ingredient)
+
+            } catch (error) {
+                console.error('Uh oh, trouble fetching single ingredient!', error);
+            }
+        }
+        fetchSingleIngredient()
+    }, [])
+};
+
+export const removeIngredient = async (ingredientId) => {
+    try {
+        const response = await fetch(`${BASE_URL}/ingredients/${ingredientId}`, {
+            method: "DELETE"
+        });
+        const result = await response.json();
+        window.location.reload();
+        return result
+        
+    } catch (err) {
         console.error(
-            `Whoops, trouble removing recipe #${recipeId}!`,
+            `Whoops, trouble removing ingredient #${ingredientId}!`,
             err
         );
     }
