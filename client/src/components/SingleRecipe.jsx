5f1c9d873c0888eeb3a483ade0db794e21263aec
Cheyenne Pantoja <CheyennePantoja@gmail.com>
2024-01-29 09:13:00 -0800

    added details

--- client/src/components/SingleRecipe.jsx
+++ client/src/components/SingleRecipe.jsx
@@ -12,7 +12,7 @@ export default function SingleRecipe() {
                 const response = await fetch(`http://localhost:8080/api/recipes/${recipeId}`);
                 const recipe = await response.json();
                 setRecipe(recipe)
-console.log(recipe)
+
             } catch (error) {
                 console.error('Uh oh, trouble fetching single recipe!', error);
             }
@@ -23,44 +23,15 @@ console.log(recipe)
     return (
         <div>
             <h2>Recipe Details</h2>
-            
             <h3>{recipe.name}</h3>
-            <h3>Description:</h3>
-            <p>{recipe.description}</p>
-            <p>Ingredient ID: {recipe.ingredientId}</p>
-            <SingleIngredient/>
-            
+            <p>Description: {recipe.description}</p>
+            <p>Ingredient #{recipe.ingredientId}</p>
+            <p>Protein: {recipe.protein}</p>
+            <p>Ingredient 1: {recipe.ingredient1}</p>
+            <p>Ingredient 2: {recipe.ingredient2}</p>    
             <br />
             <Link to={'/'}>Back to all Recipes</Link>
             <br />
         </div>
     );
 }
-
-export function SingleIngredient(ingredientId) {
-    const [ingredient, setIngredient] = useState({})
-
-    useEffect(() => {
-        async function fetchSingleIngredient() {
-            try {
-                const response = await fetch(`http://localhost:8080/api/ingredients/${ingredientId}`);
-                const ingredient = await response.json();
-                setIngredient(ingredient)
-                console.log(ingredient)
-            } catch (error) {
-                console.error('Uh oh, trouble fetching single ingredient!', error);
-            }
-        }
-        fetchSingleIngredient()
-    }, [])
-console.log(ingredient)
-    return (
-        <>
-            <h4>Ingredients</h4>
-            <p>Protein: {ingredient.protein}</p>
-            <p>Ingredient 1: {ingredient.ingredient1}</p>
-            <p>Ingredient 2: {ingredient.ingredient2}</p>
-        </>
-    );
-}
-
