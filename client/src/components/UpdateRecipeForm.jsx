da645b4c5fe1b1be847eecedfc9e8258f7f918dc
Cheyenne Pantoja <CheyennePantoja@gmail.com>
2024-01-29 09:14:25 -0800

    updated variables

--- client/src/components/UpdateRecipeForm.jsx
+++ client/src/components/UpdateRecipeForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function UpdateRecipeForm(recipe) {
+export default function UpdateRecipeForm({recipe}) {
     const [recipeId, setRecipeId] = useState('');
     const [name, setName] = useState('');
     const [description, setDescription] = useState('');
@@ -10,26 +10,25 @@ export default function UpdateRecipeForm(recipe) {
         event.preventDefault();
 
         try {
+
             const response = await fetch(`http://localhost:8080/api/recipes/${recipeId}`, {
                 method: "PUT",
                 headers: {
                     "Content-Type": "application/json"
-                }, body: JSON.stringify({
-                    recipeId: recipe.recipeId,
-                    name: recipe.name,
-                    description: recipe.description,
-                    ingredientId: recipe.ingredientId
+                }, 
+                body: JSON.stringify({
+                    name: name,
+                    description: description,
+                    ingredientId: ingredientId
                 })
             });
             const result = await response.json();
-            console.log(result);
             return result
         } catch (err) {
             console.error(err);
         }
 
         // reset form after submission
-        setRecipeId('')
         setName('')
         setDescription('')
         setIngredientId('')
@@ -38,10 +37,10 @@ export default function UpdateRecipeForm(recipe) {
     return (
         <>
             <form onSubmit={handleSubmit}>
-                <input placeholder="recipeId" type="number" value={recipeId} onChange={(e) => setRecipeId(e.target.value)} />
-                <input placeholder="name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
-                <input placeholder="description" type="text" value={description} onChange={(e) => setDescription(e.target.value)} />
-                <input placeholder="ingredientId" type="number" value={ingredientId} onChange={(e) => setIngredientId(e.target.value)} />
+                {/* <input placeholder="Recipe#" type="number" value={recipeId} onChange={(e) => setRecipeId(e.target.value)} /> */}
+                <input placeholder="Name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                <input placeholder="Description" type="text" value={description} onChange={(e) => setDescription(e.target.value)} />
+                <input placeholder="Ingredient #" type="number" value={ingredientId} onChange={(e) => setIngredientId(e.target.value)} />
                 <button type="submit">Update Recipe</button>
             </form>
         </>
