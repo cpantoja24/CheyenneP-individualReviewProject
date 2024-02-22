4b01f4802f9cd22f654572799c66e46cc619ad28
Cheyenne <149430792+cpantoja24@users.noreply.github.com>
2024-02-22 12:07:53 -0800

    Update client.js

--- server/db/client.js
+++ server/db/client.js
@@ -2,7 +2,7 @@
 const { Client } = require('pg')
 
 //Establishing the connection to the database (like how we do with http://)
-const client = new Client(`postgres://localhost:5432/recipe`)
+const client = new Client(`postgres://recipe_r5hr_user:hg5PR7kdEMf4JDEJQKApZnuISngtehXj@dpg-cnbqci6n7f5s73ahpmm0-a/recipe_r5hr`)
 
 //export for use in other files
 module.exports = client
